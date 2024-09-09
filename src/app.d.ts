fb4fcaedca4ec3e1363a2840783e7bbdeb0420d8
Othavio Quiliao <othavioaqa@gmail.com>
2024-09-09 18:53:36 -0300

    Ajustes do Google AD

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
@@ -7,6 +8,12 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+	}
+
+	// Declarando globalmente a propriedade dataLayer e a função gtag
+	interface Window {
+		dataLayer: any[];
+		gtag?: (...args: any[]) => void;
 	}
 }
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,28 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import NavBar from '$lib/components/NavBar.svelte';
-	import { ModeWatcher } from 'mode-watcher';
+	import { ModeWatcher } from 'mode-watcher'; // Certifique-se de que este pacote está instalado e funcionando corretamente.
 	import Footer from '$lib/components/Footer.svelte';
+	import { onMount } from 'svelte';
+
+	// Função gtag tipada corretamente
+	function gtag(...args: any[]) {
+		window.dataLayer.push(args);
+	}
+
+	onMount(() => {
+		const scriptTag = document.createElement('script');
+		scriptTag.src = 'https://www.googletagmanager.com/gtag/js?id=AW-11448617367';
+		scriptTag.async = true;
+		document.head.appendChild(scriptTag);
+
+		scriptTag.onload = () => {
+			window.dataLayer = window.dataLayer || [];
+			window.gtag = gtag;
+			gtag('js', new Date());
+			gtag('config', 'AW-11448617367');
+		};
+	});
 </script>
 
 <ModeWatcher defaultMode="dark" />
